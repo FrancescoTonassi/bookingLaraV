bdf1fe628d56fc665ac7b40fe97701251024a20a
FrancescoTonassi <57535330+FrancescoTonassi@users.noreply.github.com>
2025-11-26 15:18:22 +0100

    classi booking/hotel/user

--- booking-app/app/Models/User.php
+++ booking-app/app/Models/User.php
@@ -21,7 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
+
+    public function bookings() {
+    return $this->hasMany(Booking::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
